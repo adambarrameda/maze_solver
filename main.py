46e597cf5f998ad469e0f0023378a8b49bd65950
adambarrameda <hello@adambarrameda.com>
2023-12-17 23:39:24 -0500

    I made a line

--- main.py
+++ main.py
@@ -21,8 +21,33 @@ class Window:
     def close(self):
         self.running = False
 
+    def draw_line(self, line, color):
+        line.draw(self.canvas, color)
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_a, point_b):
+        self.a = point_a
+        self.b = point_b
+
+    def draw(self, canvas, color):
+        canvas.create_line(
+            self.a.x, self.a.y, self.b.x, self.b.y, fill=color, width=2
+        )
+        canvas.pack()
+
+
 def main():
     win = Window(800, 600)
+    first = Point(51, 22)
+    second = Point(300, 102)
+    big_line = Line(first, second)
+    big_line.draw(win.canvas, 'black')
     win.wait_for_close()
 
 main()
